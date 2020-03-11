573430855129d12e7b511a27f153e10342ae7ae7
malarm <malarm09@gmail.com>
2020-03-11 10:17:10 +0100

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,10 +19,10 @@ class Projects extends Component {
             <CardTitle  >Search and Display data from API</CardTitle>
             <CardText>
               <p> This website is made for a company which rents out summer houses to its clients. There are two main features in this website.
-              The first feature is to show the property details .Using my front end skills I help the product owner (company owner) show property
+              The first feature is to show the property details .Using my front-end skills I help the product owner (company owner) show property
                  details such as property picture, number of bedrooms, guest counts, city and price per night. </p>
-              <p>The second important feature is to show price range in the filter. As the product owner has umpteen number of summer houses.
-                 I have added the price range in the filter, in order to help the clients  select the summer houses based on their affordability.</p>
+              <p>The second important feature is to show tarrif range in the filter. As the product owner has umpteen number of summer houses,
+                 I have added the tarrif range in the filter, in order to help the clients  select the summer houses based on their affordability.</p>
               <p>Used Technologies : React, HTML5, CSS3 And API</p>
             </CardText>
             <CardActions border>
@@ -36,7 +36,7 @@ class Projects extends Component {
           <Card shadow={5} style={{ height: 'min-content' }}>
             <CardTitle >Malar Resort</CardTitle>
             <CardText>
-              <p> I developed a website for a resort. The search function is the main function of the website.
+              <p> I have developed a website for a resort. The search function is the main function of the website.
               Hotel rooms can be selected based on room type, number of guests and the price of the rooms.
                Web API is an important technical feature.</p>
               <p>Used Technologies : React, React Router, React Icons, HTML5, CSS3 And contentful API</p>
@@ -59,7 +59,11 @@ class Projects extends Component {
           <Card shadow={5}>
             <CardTitle  >Search Movies from IMDB </CardTitle>
             <CardText>
-              <p> I developed a website to learn to use web API when I develop. It was to challenge myself as I never worked with web API before. The website has a search function where we can insert one or many IMDB IDs. When a IMDB ID is inserted for the first time, a web API  is used to bring information from the third party website. Once a IMDB is checked  for the first time, the data is stored in backend of  local website. It means that when a IMDB ID is inserted for the second time, the data comes from the local backend, not the third part website. It was a good learning process. </p>
+              <p> I have developed a website to learn to use web API. It was to challenge myself as I never worked with web API before.
+              The website has a search function where we can insert one or many IMDB IDs. When a IMDB ID is inserted for the first time,
+              a web API  is used to bring information from the third party website. Once a IMDB is checked  for the first time, the data is
+              stored in the backend of local website. It means that when a IMDB ID is inserted for the second time, the data comes from the
+                  local backend, not the third party website. </p>
               <p>Used Technologies : C#, ASP.NET.Core, API, Entity framework, MS  SQL, Bootstrap</p>
             </CardText>
             <CardActions border >
@@ -72,8 +76,6 @@ class Projects extends Component {
 
   }
 
-
-
   render() {
     return (
       <div>
@@ -81,7 +83,6 @@ class Projects extends Component {
           <Tab>React</Tab>
           <Tab>.Net</Tab>
         </Tabs>
-
 
         <Grid>
           <Cell col={12}>
